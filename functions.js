fc75ee5d9d5e90f3ddd58bdb82c2cb177ec9da65
Sophia Howson <sophiahowson@gmail.com>
2019-01-03 13:21:22 -0500

    display

---
+++ functions.js
@@ -0,0 +1,76 @@
+
+clear() {
+
+}
+
+plusMinus() {
+
+}
+
+mod() {
+
+}
+
+division() {
+
+}
+
+num7() {
+
+}
+
+num8() {
+
+}
+
+num9() {
+
+}
+
+mul() {
+
+}
+
+num4() {
+
+}
+
+num5() {
+
+}
+
+num6() {
+
+}
+
+subtract() {
+
+}
+
+num1() {
+
+}
+
+num2() {
+
+}
+
+num3() {
+
+}
+
+add() {
+
+}
+
+zero() {
+
+}
+
+deci() {
+
+}
+
+equal() {
+
+}
