a82809d581d22340ba12c4ef7a82bb9db1c6da21
Sophia Howson <sophiahowson@gmail.com>
2019-01-11 13:17:23 -0500

    array works

--- functions.js
+++ functions.js
@@ -3,8 +3,8 @@ let numbers = [];
 let p = document.getElementById("display");
 
 function clear() {
- for(let i = 0; i < numbers.length; i++) {
-    let deleted = numbers.pop;
+ for (let i = 0; i < numbers.length; i++) {
+    numbers.pop();
  }
 }
 
@@ -21,16 +21,24 @@ function division() {
 }
 
 function num7() {
-    numbers.add = 7;
-    document.getElementById("display").innerHTML = `${numbers}`;
+  if(numbers.length < 9) {
+    numbers.push(7);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num8() {
-
+  if(numbers.length < 9) {
+    numbers.push(8);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num9() {
-
+  if(numbers.length < 9) {
+    numbers.push(9);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function mul() {
@@ -38,15 +46,24 @@ function mul() {
 }
 
 function num4() {
-
+  if(numbers.length < 9) {
+    numbers.push(4);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num5() {
-
+  if(numbers.length < 9) {
+    numbers.push(5);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num6() {
-
+  if(numbers.length < 9) {
+    numbers.push(6);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function subtract() {
@@ -54,15 +71,24 @@ function subtract() {
 }
 
 function num1() {
-
+  if(numbers.length < 9) {
+    numbers.push(1);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num2() {
-
+  if(numbers.length < 9) {
+    numbers.push(2);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function num3() {
-
+  if(numbers.length < 9) {
+    numbers.push(3);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
 }
 
 function add() {
