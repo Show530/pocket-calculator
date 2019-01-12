8bcc825f5a6006061218cf2a34140ed1da453652
Show530 <sophiahowson@gmail.com>
2019-01-12 18:38:42 -0500

    I'm going to be condensing my code, but this is an updated version before that

--- functions.js
+++ functions.js
@@ -1,73 +1,16 @@
 
 let numbers = [];
 let p = document.getElementById("display");
+document.getElementById("display").innerHTML = `0`;
 
-function clear() {
- for (let i = 0; i < numbers.length; i++) {
-    numbers.pop();
- }
-}
 
-function plusMinus() {
+//Numbers
 
-}
-
-function mod() {
-
-}
-
-function division() {
-
-}
-
-function num7() {
-  if(numbers.length < 9) {
-    numbers.push(7);
+function zero() {
+  if(numbers.length > 1 && numbers.length < 9){
+    numbers.push(0);
     document.getElementById("display").innerHTML = `${numbers.join("")}`;
   }
-}
-
-function num8() {
-  if(numbers.length < 9) {
-    numbers.push(8);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num9() {
-  if(numbers.length < 9) {
-    numbers.push(9);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function mul() {
-
-}
-
-function num4() {
-  if(numbers.length < 9) {
-    numbers.push(4);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num5() {
-  if(numbers.length < 9) {
-    numbers.push(5);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num6() {
-  if(numbers.length < 9) {
-    numbers.push(6);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function subtract() {
-
 }
 
 function num1() {
@@ -91,13 +34,92 @@ function num3() {
   }
 }
 
+function num4() {
+  if(numbers.length < 9) {
+    numbers.push(4);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+function num5() {
+  if(numbers.length < 9) {
+    numbers.push(5);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+function num6() {
+  if(numbers.length < 9) {
+    numbers.push(6);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+function num7() {
+  if(numbers.length < 9) {
+    numbers.push(7);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+function num8() {
+  if(numbers.length < 9) {
+    numbers.push(8);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+function num9() {
+  if(numbers.length < 9) {
+    numbers.push(9);
+    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+  }
+}
+
+//Operators
+function clear() {
+  //hopefully will clear array?
+  
+  /*numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+
+ console.log(numbers);
+ document.getElementById("display").innerHTML = `${numbers.join("")}`;
+*/
+}
+
+function plusMinus() {
+  
+}
+
+function mod() {
+
+}
+
+function division() {
+
+}
+
+function mul() {
+
+}
+
+
+function subtract() {
+
+}
+
 function add() {
 
 }
 
-function zero() {
-
-}
 
 function deci() {
 
