f1f20402a263dd7f2d5fff6db2d0da49ba73d5ab
Show530 <sophiahowson@gmail.com>
2019-01-12 20:02:42 -0500

    numbers work with simplfied

--- functions.js
+++ functions.js
@@ -1,98 +1,36 @@
 
 let numbers = [];
 let p = document.getElementById("display");
-document.getElementById("display").innerHTML = `0`;
 
+function iniClear() {
+  document.getElementById("display").innerHTML = "0";
+}
 
 //Numbers
 
-function zero() {
-  if(numbers.length > 1 && numbers.length < 9){
-    numbers.push(0);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num1() {
-  if(numbers.length < 9) {
-    numbers.push(1);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num2() {
-  if(numbers.length < 9) {
-    numbers.push(2);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num3() {
-  if(numbers.length < 9) {
-    numbers.push(3);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num4() {
-  if(numbers.length < 9) {
-    numbers.push(4);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num5() {
-  if(numbers.length < 9) {
-    numbers.push(5);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num6() {
-  if(numbers.length < 9) {
-    numbers.push(6);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num7() {
-  if(numbers.length < 9) {
-    numbers.push(7);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num8() {
-  if(numbers.length < 9) {
-    numbers.push(8);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
-  }
-}
-
-function num9() {
-  if(numbers.length < 9) {
-    numbers.push(9);
-    document.getElementById("display").innerHTML = `${numbers.join("")}`;
+function myButton(label) {
+  if(typeof label == 'number') {
+    if(label == 0) {
+      if(numbers.length >= 1 && numbers.length < 9){
+        numbers.push(label);
+        document.getElementById("display").innerHTML = `${numbers.join("")}`;
+      }
+    }
+    else if(numbers.length < 9) {
+      numbers.push(label);
+      document.getElementById("display").innerHTML = `${numbers.join("")}`;
+    }
   }
 }
 
 //Operators
-function clear() {
-  //hopefully will clear array?
-  
-  /*numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
+function cleer() {
+  document.getElementById("display").innerHTML = "0";
+  let l= numbers.length;
 
- console.log(numbers);
- document.getElementById("display").innerHTML = `${numbers.join("")}`;
-*/
+  for (let i=0; i < l; i++) {
+    numbers.pop();
+  }
 }
 
 function plusMinus() {
