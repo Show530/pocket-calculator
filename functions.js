83ef5c0a35e03c55770c2613a929e2dc89771175
Show530 <sophiahowson@gmail.com>
2019-01-12 21:52:00 -0500

    array to integer functions

--- functions.js
+++ functions.js
@@ -10,6 +10,7 @@ function iniClear() {
 
 function myButton(label) {
   if(typeof label == 'number') {
+    // number buttons 
     if(label == 0) {
       if(numbers.length >= 1 && numbers.length < 9){
         numbers.push(label);
@@ -19,14 +20,50 @@ function myButton(label) {
     else if(numbers.length < 9) {
       numbers.push(label);
       document.getElementById("display").innerHTML = `${numbers.join("")}`;
+    } 
+  }
+  /*
+    else if(typeof label == 'string') {
+    // four core operators
+      if(label == "division") {
+        //code
+      }
+      else if(label == "mul") {
+        //code
+      }
+      else if(label == "subtract") {
+        //code
+      }
+      else if(label == "addition") {
+        //code
+      }
+ */
     }
+}
+
+function arrayToNum1() {
+  let k = 0;
+  let n = numbers.length;
+
+  for (let i = 0; i < n; i++) {
+    k = 10 * k + numbers[i];
+  }
+}
+
+function arrayToNum2() {
+  let h = 0;
+  let n = numbers.length;
+
+  for (let i = 0; i < n; i++) {
+    h = 10 * h + numbers[i];
   }
 }
 
 //Operators
 function cleer() {
   document.getElementById("display").innerHTML = "0";
-  let l= numbers.length;
+
+  let l = numbers.length;
 
   for (let i=0; i < l; i++) {
     numbers.pop();
